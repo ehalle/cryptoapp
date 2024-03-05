7614cd9b37aaf46d0c5f6f8a276fbc0abee72b93
ehalle <eliahalle1@gmail.com>
2024-03-05 16:32:38 +0200

    * Add handle error

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import userRouter from './routers/users';
 import path from 'path';
 import config from 'config';
+import errorHandler from './middlewares/error/error-handler';
 
 const server = express();
 const port = config.get<number>('app.port');
@@ -10,7 +11,12 @@ server.set('views', path.resolve(__dirname, 'views'));
 server.set('view engine', 'ejs');
 
 server.use(express.urlencoded());
+
+//routing
 server.use('/users', userRouter);
+
+//error middlewares
+server.use(errorHandler);
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}...`);
