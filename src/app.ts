07d81bb559a3542f20dee5fbcbeebc333d264d56
ehalle <eliahalle1@gmail.com>
2024-03-05 11:22:57 +0200

    * Add ejs and types. * Add types for node. * Add dashboard html.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import userRouter from './routers/users';
+import path from 'path';
 
 const server = express();
 const PORT = 8080;
+
+server.set('views', path.resolve(__dirname, 'views'));
+server.set('view engine', 'ejs');
 
 server.use('/users', userRouter);
 
--- src/controllers/users/controller.ts
+++ src/controllers/users/controller.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 
 export function dashboard (req: Request, res: Response, next: NextFunction) {
-    console.log('dashboard');
-    res.send("hello");
+    res.render('users/dashboard');
 }
