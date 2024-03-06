aa840864a323f77253996b9d1404282533cef460
ehalle <eliahalle1@gmail.com>
2024-03-06 14:08:08 +0200

    * Add validation

---
+++ src/middlewares/input-validation.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import createHttpError, { BadRequest, InternalServerError} from "http-errors";
+import { ObjectSchema } from "joi";
+import internal from "stream";
+
+export default function validate(validator: ObjectSchema) {
+    return async function(req: Request, res: Response, next: NextFunction) {
+        try {
+            req.body = await validator.validateAsync(req.body);
+            return next();
+        } catch(err) {
+            if(err.isJoi) {
+                return next(createHttpError(BadRequest(err.message)))
+            }
+            return next(createHttpError(InternalServerError(err)));
+        }
+    }
+     
+}
