8adf53e2f71306a24788854b1f4fe456e597d0d1
ehalle <eliahalle1@gmail.com>
2024-03-06 10:06:37 +0200

    * Add mongoose.

---
+++ src/models/symbol-value/mongo.ts
@@ -0,0 +1,24 @@
+import { ObjectId } from 'mongoose';
+import { DTO } from "./dto";
+import Model from "./model";
+import mongoose from "../../db/mongo";
+
+const symbolValueSchema = new mongoose.Schema<DTO>({
+    symbol: String,
+    value: Number,
+    when: Date
+});
+
+const SymbolValueModel = mongoose.model<DTO>('SymbolValue', symbolValueSchema);
+
+class Mongo implements Model {
+    async add(symbolValue: DTO): Promise<DTO> {    
+        const newSymbolValue = new SymbolValueModel<DTO>(symbolValue);
+        await newSymbolValue.save();
+        console.log(`Symbol value created, with id ${newSymbolValue.id}`);
+        return newSymbolValue;
+    }
+}
+
+const mongo = new Mongo();
+export default mongo;
